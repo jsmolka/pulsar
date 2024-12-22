780f173e3d76b77edeb5d8f6a16d4ce24196be76
jsmolka <jsmolka@pm.me>
2024-12-22 22:11:31 +0100

    wip graphic

--- src/views/Index.vue
+++ src/views/Index.vue
@@ -19,7 +19,7 @@ import pulsar from '@/assets/data/pulsar.json';
 import { Button } from '@/components/ui/button';
 import { Graphic } from '@/graphic/graphic';
 import * as THREE from '@/graphic/three';
-import { degToRad, vec2, vec3 } from '@/graphic/three';
+import { degToRad, vec3 } from '@/graphic/three';
 import { useSettingsStore } from '@/stores/settings';
 import { colors } from '@/utils/colors';
 import SettingsDialog from '@/views/SettingsDialog.vue';
@@ -32,60 +32,53 @@ import { onBeforeUnmount, onMounted, useTemplateRef, watch } from 'vue';
 const { settings } = storeToRefs(useSettingsStore());
 
 class PulsarGraphic extends Graphic {
-  curve(data) {
-    if (settings.value.interpolate === 0) {
-      const points = data.map((y, x) => vec2(x, y));
-      return new THREE.Path3d(points);
-    }
-    if (settings.value.interpolate > 0) {
-      const points = data.map((y, x) => vec3(x, y, 0));
-      return new THREE.CatmullRomCurve3(points);
-    }
-    if (settings.value.interpolate < 0) {
-      let points = data.map((y, x) => vec2(x, y));
-      points = points.filter((_, index) => index % Math.abs(settings.value.interpolate) === 0);
-      return new THREE.Path3d(points);
-    }
-    return new THREE.Path3d();
-  }
-
   wave(data) {
     const group = new THREE.Group();
-    const curve = this.curve(data);
+
+    let points = data.map((y, x) => vec3(x, y, 0));
+    if (settings.value.interpolate > 0) {
+      const curve = new THREE.CatmullRomCurve3(points);
+      points = curve.getPoints(settings.value.interpolate * points.length);
+    } else if (settings.value.interpolate < 0) {
+      const mod = Math.abs(settings.value.interpolate);
+      points = points.filter((_, index) => index % mod === 0);
+    }
 
     // Background
     {
-      const geometry = new THREE.ShapeGeometry(
-        new THREE.Shape(curve.getPoints(settings.value.curveSteps)),
-      );
+      const geometry = new THREE.ShapeGeometry(new THREE.Shape(points));
       geometry.rotateX(degToRad(90));
-      const material = new THREE.MeshBasicMaterial({
-        color: colors.shade8.int,
-        side: THREE.DoubleSide,
-      });
-      const mesh = new THREE.Mesh(geometry, material);
-      group.add(mesh);
+
+      group.add(
+        new THREE.Mesh(
+          geometry,
+          new THREE.MeshBasicMaterial({
+            color: colors.shade8.int,
+            side: THREE.DoubleSide,
+          }),
+        ),
+      );
     }
 
     // Curve
     {
-      const circle = new THREE.Shape();
-      circle.moveTo(settings.value.lineWidth / 2, 0);
-      circle.absarc(0, 0, settings.value.lineWidth / 2, 0, degToRad(360), false);
-
-      const geometry = new THREE.ExtrudeGeometry(circle, {
-        steps: settings.value.curveSteps,
-        extrudePath: curve,
-        curveSegments: settings.value.curveSegments,
-        bevelEnabled: false,
-      });
+      const positions = [];
+      for (const point of points) {
+        positions.push(point.x, point.y, point.z);
+      }
+      const geometry = new THREE.LineGeometry().setPositions(positions);
       geometry.rotateX(degToRad(90));
-      const material = new THREE.MeshBasicMaterial({
-        color: colors.shade1.int,
-      });
 
-      const line = new THREE.Mesh(geometry, material);
-      group.add(line);
+      group.add(
+        new THREE.Line2(
+          geometry,
+          new THREE.LineMaterial({
+            color: colors.shade1.int,
+            linewidth: settings.value.lineWidth,
+            worldUnits: true,
+          }),
+        ),
+      );
     }
     return group;
   }
